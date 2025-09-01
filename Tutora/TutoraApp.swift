0863716e851098e2f754ddd9618b1634a8c278d6
marcin wawer <m.wawer.git@icloud.com>
2025-09-01 21:21:11 +0200

    chore: initial xcode project

---
+++ Tutora/TutoraApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutoraApp.swift
+//  Tutora
+//
+//  Created by Marcin Wawer on 01.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct TutoraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TutoraTests/TutoraTests.swift
@@ -0,0 +1,15 @@
+//
+//  TutoraTests.swift
+//  TutoraTests
+//
+//  Created by Marcin Wawer on 01.09.2025.
+//
+
+import Testing
+@testable import Tutora
+
+struct TutoraTests {
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+}
